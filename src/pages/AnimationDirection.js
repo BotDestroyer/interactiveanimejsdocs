7ba9552fabc19d0ba941933507d1747760048df6
BotDestroyer <jefferywei.5765ho7@gmail.com>
2022-07-13 10:50:00 -0400

    Added some animejs

--- src/pages/AnimationDirection.js
+++ src/pages/AnimationDirection.js
@@ -1,10 +1,27 @@
 import React from "react";
+import anime from "animejs";
+import '../styles/AnimationDirection.css';
 
 const AnimationDirection = function () {
-    return (
-        <h1>Animation Direction Page</h1>
-    );
-  };
+    const animationRef = React.useRef(null);
+    React.useEffect(() => {
+        animationRef.current = anime({
+            targets: ".el",
+            translateX: 250,
+            delay: function(el, i) {
+                return i * 100;
+            },
+            loop: true,
+        direction: "alternate",
+        easing: "easeInOutSine"
+    });
+  }, []);
+  return (
+    <div className="App">
+      <div className="el" />
+    </div>
+  );
+};
   
-  export default AnimationDirection;
+export default AnimationDirection;
   
